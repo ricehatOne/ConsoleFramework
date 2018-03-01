ec6170353d754017e638f5ec531d6e29df8739f4
Mathew <mathew_deery@hotmail.com>
2018-03-01 23:56:52 +1100

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "main.hpp"
+
+int main()
+{
+	try
+	{
+		init();
+		loop();
+		clean();
+	}
+	catch (...)
+	{
+
+	}
+}
+
+void init()
+{
+
+}
+
+void loop()
+{
+
+}
+
+void clean()
+{
+
+}
